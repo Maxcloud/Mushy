26f56926d1e9d106db8b7c875041b14f2c8dd60d
SharpAceX <sharpacex@gmail.com>
2016-08-31 20:35:39 -0400

    Re-recoded ChangeFmMapHandler

--- src/handling/handlers/ChangeFmMapHandler.java
+++ src/handling/handlers/ChangeFmMapHandler.java
@@ -19,7 +19,7 @@ public class ChangeFmMapHandler {
 	@PacketHandler(opcode = RecvPacketOpcode.CHANGE_FM_MAP)
 	public static void handle(MapleClient c, LittleEndianAccessor lea){
 		MapleCharacter chr = c.getPlayer();
-		
+
 		if (chr == null || chr.hasBlockedInventory() || chr.getEventInstance() != null || chr.getMap() == null || chr.isInBlockedMap() || FieldLimitType.ChannelSwitch.check(chr.getMap().getFieldLimit())) {
 			c.getSession().write(CWvsContext.enableActions());
 			return;
@@ -29,15 +29,21 @@ public class ChangeFmMapHandler {
 		int toMap = lea.readInt(); // dwFieldID
 		int localTime = lea.readInt(); // update tick
 
-		if(c.getChannel() != toChannel || !MapConstants.isFmMap(chr.getMapId()) || !MapConstants.isFmMap(toMap) || chr.getMapId() == toMap) {
-			//wrong channel or not in the fm or trying to teleport to a non-fm map or on map already
-			//they don't deserve a warning because none of these scenarios are possible without PE
+		if(!MapConstants.isFmMap(chr.getMapId()) || !MapConstants.isFmMap(toMap)) {
+			//not in the fm or trying to teleport to a non-fm map
 			c.getSession().write(CWvsContext.enableActions());
 			return;
 		}
 		
-		//you can warp them now
-		MapleMap map = ChannelServer.getInstance(c.getChannel()).getMapFactory().getMap(toMap);		
-		chr.changeMap(map, map.getPortal("out00"));
+		if (c.getChannel() != toChannel) {	
+			//cc them if they asked for it
+			chr.changeChannel(toChannel);		
+		}
+
+		if (chr.getMapId() != toMap){
+			//change map if they asked for it
+			MapleMap map = ChannelServer.getInstance(c.getChannel()).getMapFactory().getMap(toMap);		
+			chr.changeMap(map, map.getPortal("out00"));
+		}
 	}
 }
