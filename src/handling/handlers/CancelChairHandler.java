5c6a8ea57d118aaee17c8f99fddd50e61daa9b52
Tyler <admin@TylerCode.com>
2016-09-29 22:00:15 -0500

    Update CancelChairHandler.java

--- src/handling/handlers/CancelChairHandler.java
+++ src/handling/handlers/CancelChairHandler.java
@@ -10,18 +10,19 @@ public class CancelChairHandler {
 
 	@PacketHandler(opcode = RecvPacketOpcode.CANCEL_CHAIR)
 	public static void handle(MapleClient c, LittleEndianAccessor lea) {
+		int characterId = c.getPlayer().getId();
 		short id = lea.readShort();
 
 		if (id == -1) {
 			c.getPlayer().cancelFishingTask();
 			c.getPlayer().setChair(0);
-			c.getSession().write(CField.cancelChair(-1));
+			c.getSession().write(CField.cancelChair(-1, characterId));
 			if (c.getPlayer().getMap() != null) {
 				c.getPlayer().getMap().broadcastMessage(c.getPlayer(), CField.showChair(c.getPlayer().getId(), 0), false);
 			}
 		} else {
 			c.getPlayer().setChair(id);
-			c.getSession().write(CField.cancelChair(id));
+			c.getSession().write(CField.cancelChair(id, characterId));
 		}
 	}
 }
