696f8028a5093c08cfc7ebbb76d2d51523e2e419
Maxcloud <maxcloud18@hotmail.com>
2016-10-20 22:02:52 -0400

    Updated to 177.1

--- src/constants/ServerConstants.java
+++ src/constants/ServerConstants.java
@@ -2,8 +2,8 @@ package constants;
 
 public class ServerConstants {
 	
-    public static final short MAPLE_VERSION = (short) 176;
-    public static final String MAPLE_PATCH = "3";
+    public static final short MAPLE_VERSION = (short) 177;
+    public static final String MAPLE_PATCH = "1";
 
     public static enum PlayerGMRank {
 
